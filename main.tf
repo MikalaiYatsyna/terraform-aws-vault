8fab20d588d1c97f29eba3830c4c65c624b4ab80
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2023-02-22 19:05:54 +0400

    terragrunt adoption

--- main.tf
+++ main.tf
@@ -3,31 +3,31 @@ locals {
   vault_host      = "${local.vault_subdomain}.${var.domain}"
 }
 
-
 resource "aws_kms_key" "vault_unseal_key" {
   description = "Vault unseal key"
 }
+
 module "vault" {
-  source       = "app.terraform.io/logistic/vault/helm"
-  app_name     = var.app_name
-  namespace    = var.tooling_namespace
-  ingress_host = local.vault_host
+  source              = "app.terraform.io/logistic/vault/helm"
+  version             = "0.0.2"
+  app_name            = var.app_name
+  namespace           = var.tooling_namespace
+  ingress_host        = local.vault_host
   ingress_annotations = {
     "kubernetes.io/ingress.class" = "nginx"
   }
   ingress_enabled = var.create_ingress
   consul_app_name = var.consul_app_name
-  seal            = <<EOF
-                          seal "awskms" {
-                            region     = "${var.region}"
-                            kms_key_id = "${aws_kms_key.vault_unseal_key.id}"
-                          }
-                        EOF
-
-  sa_annotations = {
+  sa_annotations  = {
     "eks.amazonaws.com/role-arn"               = module.vault_role.iam_role_arn
     "eks.amazonaws.com/sts-regional-endpoints" = "true"
   }
+  seal = <<EOF
+          seal "awskms" {
+            region     = "${data.aws_region.current.name}"
+            kms_key_id = "${aws_kms_key.vault_unseal_key.id}"
+          }
+        EOF
 }
 
 module "records" {
@@ -35,7 +35,7 @@ module "records" {
   source = "terraform-aws-modules/route53/aws//modules/records"
 
   zone_name = data.aws_route53_zone.zone[0].name
-  records = [
+  records   = [
     {
       name    = local.vault_subdomain
       type    = "CNAME"
