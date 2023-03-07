c8054c07a58eba414bd4e210844def1ecad9bba6
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2023-03-07 21:05:14 +0400

    update policies
    Signed-off-by: Mikalai Yatsyna <nikolai.yatsyna@gmail.com>

--- iam.tf
+++ iam.tf
@@ -26,6 +26,25 @@ resource "aws_iam_policy" "vault_kms_policy" {
           # IAM
           "iam:GetRole",
           "iam:GetUser",
+
+          # DynamoDB for backend
+          "dynamodb:DescribeLimits",
+          "dynamodb:DescribeTimeToLive",
+          "dynamodb:ListTagsOfResource",
+          "dynamodb:DescribeReservedCapacityOfferings",
+          "dynamodb:DescribeReservedCapacity",
+          "dynamodb:ListTables",
+          "dynamodb:BatchGetItem",
+          "dynamodb:BatchWriteItem",
+          "dynamodb:CreateTable",
+          "dynamodb:DeleteItem",
+          "dynamodb:GetItem",
+          "dynamodb:GetRecords",
+          "dynamodb:PutItem",
+          "dynamodb:Query",
+          "dynamodb:UpdateItem",
+          "dynamodb:Scan",
+          "dynamodb:DescribeTable",
           # Secret Manager for populating root token
           "secretsmanager:PutSecretValue"
         ]
