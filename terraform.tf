0f1a6e57a2614eecf0a62b5e120358b244d94c9c
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2024-03-14 16:13:56 +0100

    ping versions
    Signed-off-by: Mikalai Yatsyna <nikolai.yatsyna@gmail.com>

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,9 @@
 terraform {
+  required_version = "1.7.0"
   required_providers {
-    aws        = ">= 4.57"
-    helm       = ">= 2.9.0"
-    kubernetes = ">=2.18.1"
+    aws = {
+      version = "5.40.0"
+      source  = "hashicorp/aws"
+    }
   }
 }
